b2b51203a0672ec689ccf8a1d3d26261040be5f7
DavoudTaghawiNejad <davoud@taghawi-nejad.de>
2017-08-01 11:16:16 -0300

    exposes contract variables to reserve calculation

--- insurancecompany.py
+++ insurancecompany.py
@@ -16,24 +16,26 @@ class InsuranceCompany(abce.Agent):
     def offer(self):
         free_cash = self.possession('money') - self.encumbered
         for request in self.get_messages('request_quote'):
-            provision = run_or_eval(self.reserve_formula, {'v': request.content['value']})
+            customer = (request.sender_group, request.sender_id)
+            contract = InsuranceContract(request.content['risk'],
+                                         self.riskmodel,
+                                         self.premium_formula,
+                                         request.content['value'],
+                                         (self.group, self.id),
+                                         customer,
+                                         round=self.round,
+                                         length=52)
+            provision = run_or_eval(self.reserve_formula, contract.vars)
             free_cash -= provision
             if  free_cash > 0:
-                customer = (request.sender_group, request.sender_id)
-                contract = InsuranceContract(request.content['risk'],
-                                             self.riskmodel,
-                                             self.premium_formula,
-                                             request.content['value'],
-                                             (self.group, self.id),
-                                             customer,
-                                             round=self.round,
-                                             length=52)
                 self.message(request.sender_group, request.sender_id, 'contract', contract)
+            else:
+                break
 
     def sign(self):
         for message in self.get_messages('addcontract'):
             contract = message.content
-            self.encumbered += run_or_eval(self.reserve_formula, {'v': contract.value})
+            self.encumbered += run_or_eval(self.reserve_formula, contract.vars)
             self.contracts.add(contract)
 
     def pay(self):
@@ -48,12 +50,12 @@ class InsuranceCompany(abce.Agent):
                                             r=self.round)
                 print('pay claim', self.id, obligation)
                 contract.terminated = True
-                self.encumbered -= run_or_eval(self.reserve_formula, {'v': contract.value})
+                self.encumbered -= run_or_eval(self.reserve_formula, contract.vars)
 
     def unencumber(self):
         for contract in self.contracts:
             if contract.check_ended(self.round):
-                self.encumbered -= run_or_eval(self.reserve_formula, {'v': contract.value})
+                self.encumbered -= run_or_eval(self.reserve_formula, contract.vars)
         self.log('money', self.possession('money'))
         self.log('encumbered', self.encumbered)
         self.log('free_cash', self.possession('money') - self.encumbered)
--- insurancecontract.py
+++ insurancecontract.py
@@ -10,13 +10,15 @@ class InsuranceContract(Contract):
         b = riskmodel.estimate_b(risk.b)
 
         probability_estimate = run_or_eval(riskmodel.riskmodel, {'a':a, 'b':b})
-        self.premium = run_or_eval(premium_formula, {'p': probability_estimate, 'v': value, 'l': length})
+        self.premium = run_or_eval(premium_formula, {'a':a, 'b':b, 'pe': probability_estimate, 'v': value, 'l': length})
         assert 0 <= probability_estimate <= 1, riskmodel
 
         self.add_obligation('customer', 'money', self.premium)
         self.value = value
         self.end_date = length + round
         self.terminated = False
+
+        self.vars = {'a': a, 'b': b, 'pe': probability_estimate, 'v': value, 'l': length, 'premium': self.premium}
 
     def execute(self):
         self.add_obligation('insurance_company', 'money', self.value)
